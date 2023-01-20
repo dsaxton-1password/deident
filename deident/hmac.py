9432ed1e1f666369f5e2aa55f4d1ed98d6bf9c77
Daniel Saxton <daniel.saxton@agilebits.com>
2023-01-20 15:55:46 -0600

    Add simple CI

--- deident/hmac.py
+++ deident/hmac.py
@@ -7,5 +7,6 @@ if not key:
     raise RuntimeError("HMAC_SECRET environment variable must be set")
 key = bytes(key, "utf-8")
 
+
 def apply(messages: list) -> list:
     return [hmac.digest(key, bytes(m, "utf-8"), hashlib.sha256).hex() for m in messages]
