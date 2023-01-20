a24f4f4a2b4468e845e04dc6c433b4e7497393a3
Daniel Saxton <daniel.saxton@agilebits.com>
2023-01-20 09:49:27 -0600

    Move key fetching

--- deident/hmac.py
+++ deident/hmac.py
@@ -2,9 +2,10 @@ import os
 import hmac
 import hashlib
 
+key = os.environ.get("HMAC_SECRET")
+if not key:
+    raise RuntimeError("HMAC_SECRET environment variable must be set")
+key = bytes(key, "utf-8")
+
 def apply(messages: list) -> list:
-    key = os.environ.get("HMAC_SECRET")
-    if not key:
-        raise RuntimeError("HMAC_SECRET environment variable must be set")
-    key = bytes(key, "utf-8")
     return [hmac.digest(key, bytes(m, "utf-8"), hashlib.sha256).hex() for m in messages]
