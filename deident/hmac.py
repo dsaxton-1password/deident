30b0d0b108cc54f3843cf7c17a40b88a9c41f5b3
Daniel Saxton <daniel.saxton@agilebits.com>
2023-01-26 11:26:24 -0600

    Remove unnecessary code
    secretsmanager.get_secret_value will raise already when the value
    isn't found

--- deident/hmac.py
+++ deident/hmac.py
@@ -8,10 +8,6 @@ key = os.environ.get("HMAC_SECRET")
 if not key:
     secretsmanager = boto3.client("secretsmanager")
     key = secretsmanager.get_secret_value(SecretId="TODO")
-    if not key:
-        raise RuntimeError(
-            "Could not find HMAC key in either HMAC_SECRET environment variable or SecretsManager"
-        )
 key = bytes(key, "utf-8")
 
 
